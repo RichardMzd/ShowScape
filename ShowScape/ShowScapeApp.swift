c6bff801451c7fb56afa1e55b0768a561231b814
Richard Mazid <Richard.mazid@gmail.com>
2024-04-24 13:49:18 +0200

    Initial Commit

---
+++ ShowScape/ShowScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowScapeApp.swift
+//  ShowScape
+//
+//  Created by Richard Arif Mazid on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShowScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
